7c092c346c3b90fd095f90cbb31a72f7caf7fbc0
Tycho Laptop <tychozaal@gmail.com>
2023-02-04 02:35:13 +0100

    Acceleration enzo

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5;
+    private float speed = 5, acceleration = 3, decceleration = 2, velPower = 1;
 
     private Rigidbody rb;
 
@@ -21,7 +21,15 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         var moveDirection = rb.transform.forward * movementInput.y + rb.transform.right * movementInput.x;
-        rb.AddForce(moveDirection.normalized * speed, ForceMode.Acceleration);
+        moveDirection = moveDirection * speed;
+
+        Vector2 speedDiff = new Vector2(moveDirection.x - rb.velocity.x, moveDirection.z - rb.velocity.z);
+        Vector2 accelerate = new Vector2((Mathf.Abs(moveDirection.x) > 0.01f) ? acceleration : decceleration, (Mathf.Abs(moveDirection.z) > 0.01f) ? acceleration : decceleration);
+        float movementX = Mathf.Pow(Mathf.Abs(speedDiff.x) * accelerate.x, velPower) * Mathf.Sign(speedDiff.x);
+        float movementZ = Mathf.Pow(Mathf.Abs(speedDiff.y) * accelerate.y, velPower) * Mathf.Sign(speedDiff.y);
+        Vector3 movement = new Vector3(movementX, 0, movementZ);
+
+        rb.AddForce(movement);
     }
 
     public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
